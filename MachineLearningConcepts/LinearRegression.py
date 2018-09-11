7dd71ac4c68004b1fb2c03522e7d9ab378d1723e
Brian Rodriguez <brian.rodriguez17@upr.edu>
2018-09-10 21:30:09 -0400

    Modified alpha to converge. Still looking into best alpha to use.

--- MachineLearningConcepts/LinearRegression.py
+++ MachineLearningConcepts/LinearRegression.py
@@ -1,14 +1,10 @@
-test_features = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-test_results = [3, 5, 7, 9, 11, 13, 15, 17, 19, 21]
-theta0 = 7
-theta1 = 6
-alpha = 0.01
-
-
 class Data(object):
     """An object to hold the required dataset data."""
 
-    def __init__(self, features, results, theta0=0, theta1=1, alpha=0.001):
+    def __init__(self, features, results, theta0=0, theta1=1, alpha=0.00001):
+        """Initialize data, hypothesis, and Gradient descent variables
+        """
+
         self.features = features
         self.results = results
         self.theta0 = theta0
@@ -16,6 +12,9 @@ class Data(object):
         self.alpha = alpha
 
     def print_info(self):
+        """Prints Current Data Info
+        """
+
         print("Features:" + self.features.__str__())
         print("Results:" + self.results.__str__())
         print("Theta0 =" + self.theta0.__str__())
@@ -23,15 +22,20 @@ class Data(object):
         print(self.cost())
 
     def cost(self):
+        """Prints the current cost of the hypothesis.
+        """
+
         print("COST METHOD:")
         sum_right = 0
         n = 0
-
-        while n < len(self.features):
-            sum_right = sum_right + (
-                pow(self.hypothesis_minus_actual_value(feature=self.features[n], actual_value=self.results[n]), 2))
-            n = n + 1
-        cost_result = sum_right / (2 * (len(self.features)))
+        length_of_features = len(self.features)
+        # The sum of the right hand side of the Cost function:
+        # sum of the square of the hypothesis of x sub i minus the actual value for x sub i, for all i in the features.
+        while n < length_of_features:
+            sum_right += pow(self.hypothesis_minus_actual_value(feature=self.features[n],
+                                                                actual_value=self.results[n]), 2)
+            n += 1
+        cost_result = sum_right / (2 * length_of_features)
 
         return cost_result
 
@@ -41,49 +45,44 @@ class Data(object):
     def hypothesis_minus_actual_value(self, feature, actual_value):
         return self.hypothesis(feature=feature) - actual_value
 
-    def gradient_descent_0(self, theta0):
+    def gradient_descent_0(self):
         sum_right = 0
         n = 0
 
         while n < len(self.features):
-            sum_right = sum_right + self.hypothesis_minus_actual_value(feature=self.features[n],
-                                                                       actual_value=self.results[n])
+            sum_right += self.hypothesis_minus_actual_value(feature=self.features[n],
+                                                            actual_value=self.results[n])
             n = n + 1
-        sum_right = sum_right * alpha
-        sum_right = sum_right / len(self.features)
+        sum_right = (sum_right * self.alpha)/len(self.features)
 
-        return theta0 - sum_right
+        return self.theta0 - sum_right
 
-    def gradient_descent_1(self, theta1):
+    def gradient_descent_1(self):
         sum_right = 0
         n = 0
 
-        while n < len(test_features):
-            sum_right = sum_right + (
-                    self.hypothesis_minus_actual_value(feature=self.features[n], actual_value=self.results[n]) *
-                    self.features[n])
+        while n < len(self.features):
+            sum_right += self.hypothesis_minus_actual_value(feature=self.features[n],
+                                                            actual_value=self.results[n]) * self.features[n]
             n = n + 1
-        sum_right = sum_right * alpha
-        sum_right = sum_right / len(self.features)
+        sum_right = sum_right * self.alpha/ len(self.features)
 
-        return theta1 - sum_right
+        return self.theta1 - sum_right
 
-    def batch_gradient_descent(self, threshold=0.01):
+    def batch_gradient_descent(self, threshold=0.00001):
 
-        lim = 100000
         old_theta0 = self.theta0
         old_theta1 = self.theta1
         has_not_run_yet = True
         counter = 0
-        while counter < lim:
-            temp0 = self.gradient_descent_0(self.theta0)
-            temp1 = self.gradient_descent_1(self.theta1)
+        while has_not_run_yet or ((abs(old_theta0-self.theta0)> threshold) or (abs(old_theta1-self.theta1)> threshold)) :
+            temp0 = self.gradient_descent_0()
+            temp1 = self.gradient_descent_1()
             old_theta0 = self.theta0
             old_theta1 = self.theta1
             self.theta0 = temp0
             self.theta1 = temp1
             has_not_run_yet = False
-            counter += 1
 
         print("New Thetas: " + self.theta0.__str__() + ", " + self.theta1.__str__())
 
