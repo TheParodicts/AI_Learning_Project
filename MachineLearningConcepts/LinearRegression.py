f3842f3476ce903727eb875e30a7aa30e621e68d
Brian Rodriguez <brian.rodriguez17@upr.edu>
2018-09-13 16:52:44 -0400

    Commented LinearRegression Methods

--- MachineLearningConcepts/LinearRegression.py
+++ MachineLearningConcepts/LinearRegression.py
@@ -29,53 +29,96 @@ class Data(object):
         sum_right = 0
         n = 0
         length_of_features = len(self.features)
+
         # The sum of the right hand side of the Cost function:
         # sum of the square of the hypothesis of x sub i minus the actual value for x sub i, for all i in the features.
         while n < length_of_features:
             sum_right += pow(self.hypothesis_minus_actual_value(feature=self.features[n],
                                                                 actual_value=self.results[n]), 2)
             n += 1
+
+        # Divide the sum by twice the length of features.
         cost_result = sum_right / (2 * length_of_features)
 
         return cost_result
 
     def hypothesis(self, feature):
+        """Calculate the guess based on the current thetas.
+
+        :param float feature: variable used to calculate guess.
+        :return: float guess: the result of the hypothesis
+        """
         return self.theta0 + (self.theta1 * feature)
 
     def hypothesis_minus_actual_value(self, feature, actual_value):
+        """Subtracts the actual value for a feature from the hypothesis calculated result of said feature.
+
+        :param float feature: feature tu use.
+        :param float actual_value: actual value corresponding to feature
+        :return: hypothesis result minus actual value
+        :rtype: float
+        """
         return self.hypothesis(feature=feature) - actual_value
 
     def gradient_descent_0(self):
+        """Calculates a single step in gradient descent for theta 0.
+
+        :return: gradient descent result for theta 0.
+        :rtype: float
+        """
+
         sum_right = 0
         n = 0
 
+        # Sumation of the hypothesis minus actual value for every feature in data set.
         while n < len(self.features):
             sum_right += self.hypothesis_minus_actual_value(feature=self.features[n],
                                                             actual_value=self.results[n])
             n = n + 1
-        sum_right = (sum_right * self.alpha)/len(self.features)
 
+        # Multiply sum by alpha and divide by number of data elements.
+        sum_right = (sum_right * self.alpha) / len(self.features)
+
+        # Return old theta 0 minus right hand side of gradient descent.
         return self.theta0 - sum_right
 
     def gradient_descent_1(self):
+        """Calculates a single step in gradient descent for theta 1.
+
+        :return: gradient descent result for theta 1.
+        :rtype: float
+        """
         sum_right = 0
         n = 0
 
+        # Sumation of the hypothesis minus actual value for every feature in data set times the feature.
         while n < len(self.features):
             sum_right += self.hypothesis_minus_actual_value(feature=self.features[n],
                                                             actual_value=self.results[n]) * self.features[n]
             n = n + 1
-        sum_right = sum_right * self.alpha/ len(self.features)
 
+        # Multiply sum by alpha and divide by number of data elements.
+        sum_right = sum_right * self.alpha / len(self.features)
+
+        # Return old theta 0 minus right hand side of gradient descent.
         return self.theta1 - sum_right
 
     def batch_gradient_descent(self, threshold=0.00001):
+        """Calculates batch gradient descent for both thetas to a certain degree of precision.
+
+        Calculates the batch gradient descent for both  theta 0 and theta 1, then updates them
+            simultaneously, until the difference is less than a threshold.
+
+        :param float threshold: threshold value for which difference between new theta and old theta should be
+            less than.
+        """
 
         old_theta0 = self.theta0
         old_theta1 = self.theta1
         has_not_run_yet = True
-        counter = 0
-        while has_not_run_yet or ((abs(old_theta0-self.theta0)> threshold) or (abs(old_theta1-self.theta1)> threshold)) :
+        while has_not_run_yet or (
+                (abs(old_theta0 - self.theta0) > threshold) or
+                (abs(old_theta1 - self.theta1) > threshold)):
             temp0 = self.gradient_descent_0()
             temp1 = self.gradient_descent_1()
             old_theta0 = self.theta0
@@ -85,8 +128,3 @@ class Data(object):
             has_not_run_yet = False
 
         print("New Thetas: " + self.theta0.__str__() + ", " + self.theta1.__str__())
-
-# test_data_object = Data(features=test_features, results=test_results, theta0=theta0, theta1=theta1, alpha=alpha)
-# test_data_object.print_info()
-# test_data_object.batch_gradient_descent()
-# test_data_object.print_info()
